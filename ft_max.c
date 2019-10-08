d53b3ae7a0f585b1caf11b8a34d3e84d05a3651e
nneuda <natallia.neuda@gmail.com>
2019-10-07 21:18:09 -0700

    123

--- ft_max.c
+++ ft_max.c
@@ -6,16 +6,16 @@
 /*   By: nneuda <nneuda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 17:18:29 by nneuda            #+#    #+#             */
-/*   Updated: 2019/10/07 17:34:16 by nneuda           ###   ########.fr       */
+/*   Updated: 2019/10/07 21:17:28 by nneuda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	max(int *tab, unsigned int len)
+int	ft_max(int *tab, unsigned int len)
 {
-	unsigned int i;
-	int max;
+	unsigned int 	i;
+	int 			max;
 
 	i = 0;
 	max = tab[i];
